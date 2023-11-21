e981ff1c5561c2fef16052f2b2bedf72cf052414
RockhillMarcos <103252825+RockhillMarcos@users.noreply.github.com>
2023-11-21 05:51:14 +0400

    Update e.sol
    Added contrucror

--- e.sol
+++ e.sol
@@ -10,4 +10,12 @@ contract SimpleToken {
 
     event Transfer(address indexed from, address indexed to, uint256 value);
 
+constructor(string memory _name, string memory _symbol, uint256 _initialSupply) {
+        name = _name;
+        symbol = _symbol;
+        totalSupply = _initialSupply;
+        balanceOf[msg.sender] = _initialSupply;
+    }
+
+
 }
