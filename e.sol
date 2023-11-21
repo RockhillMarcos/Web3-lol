124d3bffc4ec316b0d50a37724c8f9deb43a8ee5
RockhillMarcos <103252825+RockhillMarcos@users.noreply.github.com>
2023-11-21 05:50:54 +0400

    Update e.sol
    Added base

--- e.sol
+++ e.sol
@@ -1,1 +1,13 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SimpleToken {
+    string public name;
+    string public symbol;
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+}
